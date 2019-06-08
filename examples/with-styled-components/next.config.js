f35d14197f07f05ec49db71032245d9a482a6276
Faris Abusada <faris.a@yamsafer.me>
2019-06-08 11:59:49 +0300

    remove log

--- examples/with-styled-components/next.config.js
+++ examples/with-styled-components/next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   babel: ({ babelConfig, options }) => {
     babelConfig.plugins.push(['styled-components', { ssr: true }])
-    console.log(babelConfig)
     return babelConfig
   }
 }
